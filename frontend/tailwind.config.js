43065a1a29d0abfa6754f837e5ba038755c8ec42
Geerthanah Mathivathanan <138343450+geerthanah@users.noreply.github.com>
2025-07-06 09:52:48 +0530

    files update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // scan these files for Tailwind classes
-  ],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
